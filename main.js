7b108262e510b18413e7ba9f5e60d8a06f783b02
jev11 <evgeny.barannikov@gmail.com>
2021-06-11 15:10:57 +0000

    Deploying to gh-pages from @ jev11/otus-weather-forecast@6f3b39a1d790c9ac2467e3c89d95e61ae681ec6d 🚀

--- main.js
+++ main.js
@@ -1,1 +1,141 @@
-(()=>{"use strict";function t(t,n){t.innerHTML=`<ul>${n.map((t=>`<li class="city">${t}</li>`)).join("")}</ul>`}function n(t,n){t.innerHTML=`<div>\n    <span>\n    ${n.name}\n    </span>\n    <span>\n    <img src=https://openweathermap.org/img/wn/${n.weather[0].icon}.png>\n    </span>\n    <span>\n    Temperature: ${Math.round(n.main.temp)}C\n    </span>\n    </div>`}async function e(t,n){try{const e=`https://api.openweathermap.org/\ndata/2.5/weather?units=metric&q=${t}&appid=${n}`,a=await fetch(e);return await a.json()}catch(t){return null}}function a(t,n,e,a){const c=`https://maps.googleapis.com/maps/api/staticmap?center=\n${n},${e}&zoom=12&size=400x400&key=${a}`;t.innerHTML=`<img src="${c}">`}const c="69c7e4c421845cddf495f67b63f6cc51",o="AIzaSyC9RYvd4Srwv8p0dEjkPUnHPRrKouz5lfQ",r=document.querySelector(".history"),i=document.querySelector("input"),s=document.querySelector("button"),l=document.querySelector(".forecast"),u=document.querySelector(".map");let p,d;!async function(){const i=await async function(t="https://get.geojs.io/v1/ip/geo.json"){try{const n=await fetch(t);return await n.json()}catch(t){return null}}();d=await e(i.city,c),d&&(n(l,d),a(u,d.coord.lat,d.coord.lon,o)),p=await(JSON.parse(localStorage.getItem("cities"))||[]),t(r,p),r.querySelectorAll(".city").forEach((t=>{t.addEventListener("click",(async()=>{d=await e(t.innerHTML,c),d&&(n(l,d),a(u,d.coord.lat,d.coord.lon,o))}))}))}(),s.addEventListener("click",(async s=>{var m,y;s.preventDefault(),d=await e(i.value,c),d&&(m=i.value,(y=p).unshift(m),y.length>10&&y.pop(),n(l,d),a(u,d.coord.lat,d.coord.lon,o),t(r,p)),i.value="",r.querySelectorAll(".city").forEach((t=>{t.addEventListener("click",(async()=>{d=await e(t.innerHTML,c),d&&(n(l,d),a(u,d.coord.lat,d.coord.lon,o))}))})),function(t){const n=JSON.stringify(t);localStorage.setItem("cities",n)}(p)}))})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/history/index.js
+function loadHistory() {
+  const cities = JSON.parse(localStorage.getItem("cities"));
+  if (cities) {
+    return cities;
+  }
+  return [];
+}
+
+function saveHistory(list) {
+  const objJson = JSON.stringify(list);
+  localStorage.setItem("cities", objJson);
+}
+
+function drawHistory(el, list) {
+  const newEl = el;
+  newEl.innerHTML = `<ul>${list
+    .map((item) => `<li class="city">${item}</li>`)
+    .join("")}</ul>`;
+}
+
+function keepHistory(city, list) {
+  list.unshift(city);
+  if (list.length > 10) {
+    list.pop();
+  }
+}
+
+;// CONCATENATED MODULE: ./src/weather/index.js
+function drawWeather(el, weatherInfo) {
+  const newEl = el;
+  newEl.innerHTML = `<div>
+    <span>
+    ${weatherInfo.name}
+    </span>
+    <span>
+    <img src=https://openweathermap.org/img/wn/${
+      weatherInfo.weather[0].icon
+    }.png>
+    </span>
+    <span>
+    Temperature: ${Math.round(weatherInfo.main.temp)}C
+    </span>
+    </div>`;
+}
+
+async function getWeather(cityName, key) {
+  try {
+    const url = `https://api.openweathermap.org/
+data/2.5/weather?units=metric&q=${cityName}&appid=${key}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
+  } catch (e) {
+    return null;
+  }
+}
+
+;// CONCATENATED MODULE: ./src/location/index.js
+async function getCurrentLocation(
+  url = "https://get.geojs.io/v1/ip/geo.json"
+) {
+  try {
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
+  } catch (e) {
+    return null;
+  }
+}
+
+function drawMap(el, lat, lon, key) {
+  const newEl = el;
+  const url = `https://maps.googleapis.com/maps/api/staticmap?center=
+${lat},${lon}&zoom=12&size=400x400&key=${key}`;
+  newEl.innerHTML = `<img src="${url}">`;
+}
+
+;// CONCATENATED MODULE: ./src/index.js
+// при открытии страницы пользователь видит погоду (город, температуру и иконку) в своей местности (для получения прогноза погоды используйте https://openweathermap.org/current )
+// он может ввести имя города в поле ввода и увидеть погоду в выбранном городе
+// введенные города сохраняются у пользователя в браузере, так что он видит последние 10 городов, где он смотрел погоду
+// при клике по строчке города в списке он видит погоду в выбранном городе
+// кроме информации о погоде покажите в центре страницы карту для введенного адреса (используйте Google Maps Static API https://developers.google.com/maps/documentation/maps-static/start )
+// Проверить покрытие кода тестами, и добавить проверку покрытия при запуске test скрипта. Покрытие должно быть не ниже 60%
+
+
+
+
+
+const owKey = "69c7e4c421845cddf495f67b63f6cc51";
+const gmKey = "AIzaSyC9RYvd4Srwv8p0dEjkPUnHPRrKouz5lfQ";
+
+const historyEl = document.querySelector(".history");
+const input = document.querySelector("input");
+const src_button = document.querySelector("button");
+const forecastEl = document.querySelector(".forecast");
+const mapEl = document.querySelector(".map");
+
+let citiesList;
+let weather;
+(async function renderStartPage() {
+  const currentLocation = await getCurrentLocation();
+  weather = await getWeather(currentLocation.city, owKey);
+  if (weather) {
+    drawWeather(forecastEl, weather);
+    drawMap(mapEl, weather.coord.lat, weather.coord.lon, gmKey);
+  }
+
+  citiesList = await loadHistory();
+  drawHistory(historyEl, citiesList);
+  historyEl.onclick = async function delegateEventonHistoryEl(event) {
+    weather = await getWeather(event.target.innerHTML, owKey);
+    if (weather) {
+      drawWeather(forecastEl, weather);
+      drawMap(mapEl, weather.coord.lat, weather.coord.lon, gmKey);
+    }
+  };
+})();
+
+src_button.addEventListener("click", async (ev) => {
+  ev.preventDefault();
+
+  weather = await getWeather(input.value, owKey);
+  if (weather) {
+    keepHistory(input.value, citiesList);
+    drawWeather(forecastEl, weather);
+    drawMap(mapEl, weather.coord.lat, weather.coord.lon, gmKey);
+    drawHistory(historyEl, citiesList);
+  }
+  input.value = "";
+
+  saveHistory(citiesList);
+});
+
+/******/ })()
+;
